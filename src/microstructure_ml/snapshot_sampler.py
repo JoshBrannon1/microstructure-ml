092cfaaa699f38abac01585718752ba0dc8124dc
JoshBrannon1 <brannonjosh@icloud.com>
2026-04-05 23:27:55 -0400

    Fix snapshot sampler bugs

--- src/microstructure_ml/snapshot_sampler.py
+++ src/microstructure_ml/snapshot_sampler.py
@@ -1,32 +1,34 @@
 from microstructure_ml.book_builder import BookBuilder
-from microstructure_ml.collector import Collector
 
 def take_snapshot(book: BookBuilder, timestamp: str, product: str, exchange: str, num_levels: int) -> dict:
     snapshot = {}
-    top_bids = sorted(book.bids.keys(), reverse = True)[:10]
-    top_asks = sorted(book.asks.keys(), reverse = False)[:10]
-    num_active_bid_levels = len(top_bids)
+    top_bids = sorted(book.bids.keys(), reverse = True)[:num_levels]
+    top_asks = sorted(book.asks.keys(), reverse = False)[:num_levels]
     
+    num_active_bids = len(top_bids)
     i = 1
-    while i <= num_active_bid_levels:
-        snapshot[f"bid_price_{i}"] = top_bids[i]
-        snapshot[f"bid_size_{i}"] = book.bids[top_bids[i]]
+    while i <= num_active_bids:
+        snapshot[f"bid_price_{i}"] = top_bids[i-1]
+        snapshot[f"bid_size_{i}"] = book.bids[top_bids[i-1]]
         i += 1
     while i <= num_levels:
-        snapshot[f"bid_price{i}"] = None
-        snapshot[f"bid_size{i}"] = None
-    
-    num_active_ask_levels = len(top_asks)
-    i = 1
-    for ask in bottom_asks:
-        snapshot[f"ask_price_{i}"] = ask
-        snapshot[f"ask_size_{i}"] = book.asks[ask]
+        snapshot[f"bid_price_{i}"] = None
+        snapshot[f"bid_size_{i}"] = None
         i += 1
+    
+    num_active_asks = len(top_asks)
+    i = 1
+    while i <= num_active_asks:
+        snapshot[f"ask_price_{i}"] = top_asks[i-1]
+        snapshot[f"ask_size_{i}"] = book.asks[top_asks[i-1]]
+        i += 1
+    while i<= num_levels:
+        snapshot[f"ask_price_{i}"] = None
+        snapshot[f"ask_size_{i}"] = None
+        i += 1
+    
     snapshot["timestamp"] = timestamp
     snapshot["product"] = product
     snapshot["exchange"] = exchange
 
-
-
-    
-    
+    return snapshot
